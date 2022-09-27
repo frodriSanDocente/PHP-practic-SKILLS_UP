09c21fb572e070339c4fe33a0336a0f6c0847664
Fabián <frodriguez@fulp.es>
2022-09-27 12:03:30 +0100

    Added new folder structure.

---
+++ modules/include/new_reg.php
@@ -0,0 +1,12 @@
+<?php
+require "../require/config.php";
+
+// define variables and set to empty values
+$name = $email = $phone = $street = $city = $state = $zip = $other = $news = $newscheck =""
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+}
+
+?>

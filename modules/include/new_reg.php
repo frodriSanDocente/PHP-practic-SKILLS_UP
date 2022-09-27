df459d614e5383cf5b273cc0b18755abbe2fe547
Fabián <frodriguez@fulp.es>
2022-09-27 12:56:19 +0100

    Fixed routes.

--- modules/include/new_reg.php
+++ modules/include/new_reg.php
@@ -1,11 +1,14 @@
 <?php
 require "../require/config.php";
 
-// define variables and set to empty values
-$name = $email = $phone = $street = $city = $state = $zip = $other = $news = $newscheck =""
+// Define e inicializa las variables que se van a usar del formulario.
+$name = $email = $phone = $address = $city = $communities = $Zcode = $othert = $format = $newscheck ="";
 
+
+//Si (llega datos) Entonces
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+        echo "<br><strong>Metodo post enviado<strong><br>";
 
 }
 

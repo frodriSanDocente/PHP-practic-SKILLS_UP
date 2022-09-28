2156b0a7faa0db7327087eab9d798071cb10b123
Fabián <frodriguez@fulp.es>
2022-09-28 12:01:33 +0100

    Added new lines code.

--- modules/include/new_reg.php
+++ modules/include/new_reg.php
@@ -8,7 +8,36 @@ $name = $email = $phone = $address = $city = $communities = $Zcode = $othert = $
 //Si (llega datos) Entonces
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        echo "<br><strong>Metodo post enviado<strong><br>";
+        // echo "<br><strong>Metodo post enviado<strong><br>";
+		if (!empty($_POST["name"]) || !empty($_POST["email"]) || !empty($_POST["phone"])) {
+        	echo "<br><strong>name post hay datos<strong><br>";
+			$name = $_POST["name"];
+			$email = $_POST["email"];
+			$phone = $_POST["phone"];
+			$address = $_POST["address"];
+			$city = $_POST["city"];
+			$communities = $_POST["communities"];
+			$Zcode = $_POST["Zcode"];
+			$format = $_POST["format"];
+			$newscheck = $_POST["newscheck"];
+			$othert = $_POST["othert"];
+
+			function limpiar_dato($data){
+				$data = trim($data);
+				$data = stripslashes($data);
+				$data = htmlspecialchars($data);
+			}
+
+
+			// nombre, email y nº telefono.
+			function validar_name($name){
+				if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+					return false;
+				} else {
+					return true;
+				}
+			}
+		}
 
 }
 

d19cb2078ed46d2a26a5cbe8cf03b540cb3f2ebd
Fabián <frodriguez@fulp.es>
2022-10-03 10:06:43 +0100

    Checkeo de fallo de validaciones y reestructuración del código. Revisar

--- modules/include/new_reg.php
+++ modules/include/new_reg.php
@@ -3,6 +3,7 @@ require "../require/config.php";
 
 // Define e inicializa las variables que se van a usar del formulario.
 $name = $email = $phone = $address = $city = $communities = $Zcode = $othert = $format = $newsletter = "";
+$name_err = $email_err = $phone_err = false;
 /**
  * Función para limpiar un dato procedentes de un formulario.
  * 
@@ -75,57 +76,110 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		$name = limpiar_dato($_POST["name"]);
 		$email = limpiar_dato($_POST["email"]);
 		$phone = limpiar_dato($_POST["phone"]);
-
-		// if (isset($_POST["address"])) ? $address = limpiar_dato($_POST["address"]) : $address = NULL;
-		if (isset($_POST["address"])){
-			$address = limpiar_dato($_POST["address"]); 
-		} else{
-			$address = NULL;
+		if (validar_name($name)){
+			echo "<script> console.log('name es válido.')</script></br>";
+		} else {
+			$name_err = true;
 		}
-		$city = limpiar_dato($_POST["city"]);
-		$communities = limpiar_dato($_POST["communities"]);
-		$Zcode = limpiar_dato($_POST["Zcode"]);
-		$format = limpiar_dato($_POST["format"]);
-		$newsletter = limpiar_dato($_POST["newsletter"]);
-		//var_dump($newsletter);
-		//echo "</br>";
-		/* 			$newsletter = filter_input(
+
+		if (validar_email($email)) {
+			echo "<script> console.log('email es válido.')</script></br>"; 
+		} else {
+			$email_err = true 
+		}
+
+		if (validar_phone($phone)){
+			echo "<script> console.log('telefono es válido.')</script></br>"; 
+		} else {
+			echo "Formato telefono no válido.";
+		}
+
+		// Condicion si no se cumple, parar el código.
+		if (validar_name($name) || validar_phone($phone) || validar_email($email)) {
+			Ejecutas el resto
+			
+			// if (isset($_POST["address"])) ? $address = limpiar_dato($_POST["address"]) : $address = NULL;
+			if (isset($_POST["address"])){
+				$address = limpiar_dato($_POST["address"]); 
+			} else{
+				$address = NULL;
+			}
+			
+			if (isset($_POST["city"])){
+				$city = limpiar_dato($_POST["city"]);
+			} else {
+				$city = NULL;
+			}
+			
+			if (isset($_POST["communities"])){
+				$communities = limpiar_dato($_POST["communities"]);
+			} else {
+				$communities = NULL;
+			}
+			
+			
+			if (isset($_POST["Zcode"])){
+				$Zcode = limpiar_dato($_POST["Zcode"]);
+			} else {
+				$Zcode = NULL;
+			}
+			
+			
+			$format = limpiar_dato($_POST["format"]);
+			// $newsletter = limpiar_dato($_POST["newsletter"]);
+			//echo "</br>";
+			/* 			$newsletter = filter_input(
 				INPUT_POST,
 				'newsletter',
 				FILTER_SANITIZE_SPECIAL_CHARS,
 				FILTER_REQUIRE_ARRAY
 			); */
-		var_dump($newsletter);
-		// echo "</br>";
-
-		// === Usa un array y muestra sus valores separados por coma (o lo que se ponga entre comillas).
-		/* 	$string=implode(", ",$newsletter);
+			//var_dump($newsletter);
+			// echo "</br>";
+			
+			// === Usa un array y muestra sus valores separados por coma (o lo que se ponga entre comillas).
+			/* 	$string=implode(", ",$newsletter);
 			echo $string;
 			echo "</br>"; */
-		// === FIN MOSTRAR valores array.
-
-		$othert = limpiar_dato($_POST["othert"]);
-		echo "<strong>Noticias que quiere recibir: $newsletter";
-		var_dump($name);
-// ==========================  BORRAME
-		echo "<br><strong>Name:</strong> $name <br>";
-		echo "<strong>Telefono:</strong> $phone <br>";
-		echo "<strong>Email: </strong> $email <br>";
-
-		if (validar_name($name)){
-			echo "validada";
-		} else {
-			echo "no valida";
-		};
-// ======================  BORRAME
-
-
+			// === FIN MOSTRAR valores array.
+			
+			if (isset($_POST["othert"])){
+				$othert = limpiar_dato($_POST["othert"]);
+			} else {
+				$othert = NULL;
+			}
+			echo "<strong>Noticias que quiere recibir: $newsletter";
+			
+			// ==========================  BORRAME
+			echo "<br><strong>Name:</strong> $name <br>";
+			echo "<strong>Telefono:</strong> $phone <br>";
+			echo "<strong>Address:</strong> $address <br>";
+			echo "<strong>City: </strong> $city <br>";
+			echo "<strong>Autonomous communities: </strong> $communities <br>";
+			echo "<strong>Zip Code: </strong> $Zcode <br>";
+			echo "<strong>Newsletters: </strong> $newsletter <br>";
+			echo "<strong>Newsletter format: </strong> $format <br>";
+			echo "<strong>Other topis...: </strong> $othert <br>";
+			
+			if (validar_name($name)){
+				echo "validada";
+			} else {
+				echo "no valida";
+			};
+			// ======================  BORRAME
+			
+			
+		}
+	} else {
+		MEnsaje una de las validaciones no funciona.
 	}
+} else {
+	echo "Método post no ha llegado.";
 }
-
-/* Si (llega datos) Entonces
+	
+	/* Si (llega datos) Entonces
     tratamos datos
-		Si si hay información Entonces
+	Si si hay información Entonces
 				Si no llegan variables?**
 					Asignarles NULL.
 				limpiar la información. check!!

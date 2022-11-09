48a429e2f5e08b9251c22559d6b0b83b59fbe8c0
Fabián <frodriguez@fulp.es>
2022-11-09 08:01:42 +0000

    Create file php to show data in database. Fixed media for mobile screens.

--- public/inscritos.php
+++ public/inscritos.php
@@ -1,18 +1,12 @@
 <?php
-function read()
-{
-    $query = 'SELECT * FROM news_reg';
-
-    $stmt = $this->conn->prepare($query);
-    $stmt->execute();
-
-    $articulos = $stmt->fetchAll(PDO::FETCH_OBJ);
-    return $articulos;
-}
+require "../modules/require/config.php";
+htmlspecialchars($_SERVER['PHP_SELF']);
+$_SERVER['REQUEST_METHOD'] == null;
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -24,59 +18,84 @@ function read()
     <link rel="icon" type="image/png" href="./images/pfae.png" sizes="64x64">
 
 </head>
+
 <body>
-    <div><button type="submit">Inscritos</button></div>
+    <main>
+        <?php if ($_SERVER['REQUEST_METHOD'] === 'GET') : ?>
+            <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
+                
+                <button type="submit" name="MostrarInscritos">Usuarios Inscritos</button>
+            </form>
+            <?php else : ?>
+                <?php
+                echo "<div class='wrapper'>";
+                // require __DIR__ .  '/inc/post.php';
+                $sql = "SELECT * FROM news_reg";
+                $stmt = $conn->prepare($sql);
+                $stmt -> execute();
 
-    <div class="wrapper">
+                if ($result = $stmt->setFetchMode(PDO::FETCH_ASSOC)) {
+                    echo "<table>
+                    <thead>
+                        <tr>
+                            <th>
+                                Nombre completo
+                            </th>
+                            <th>
+                                Email
+                            </th>
+                            <th>
+                                Télefono
+                            </th>
+                            <th>
+                                Dirección
+                            </th>
+                            <th>
+                                Ciudad
+                            </th>
+                            <th>
+                                Comunidad autónoma
+                            </th>
+                            <th>
+                                Código Postal
+                            </th>
+                            <th>
+                                Boletín de noticias
+                            </th>
+                            <th>
+                                Formato
+                            </th>
+                            <th>
+                                Sugerencias
+                            </th>
+                        </tr>
+                    </thead>";
+                    // output data of each row
+                    foreach(($rows = $stmt->fetchAll()) as $row) {
+                        echo "<tr>
+                        <td>".$row["fullname"]."</td>
+                        <td>".$row["email"]."</td>
+                        <td>".$row["phone"]."</td>
+                        <td>".$row["address"]."</td>
+                        <td>".$row["city"]."</td>
+                        <td>".$row["state"]."</td>
+                        <td>".$row["zipcode"]."</td>
+                        <td>".$row["newsletters"]."</td>
+                        <td>".$row["format_news"]."</td>
+                        <td>".$row["suggestion"]."</td>
+                        </tr>";
+                    }
+                echo "</tr>
+                </table>";
+                } else {
+                    echo "0 results";
+                }
+                $conn = null;
+                ?>
+            <?php endif ?>
+        </div>
+    </main>
 
-        <table>
-            <thead>
-                <tr>
-                    <th>
-                        Nombre completo
-                    </th>
-                    <th>
-                        Email
-                    </th>
-                    <th>
-                        Télefono
-                    </th>
-                    <th>
-                        Dirección
-                    </th>
-                    <th>
-                        Ciudad
-                    </th>
-                    <th>
-                        Comunidad autónoma
-                    </th>
-                    <th>
-                        Código Postal
-                    </th>
-                    <th>
-                        Boletín de noticias
-                    </th>
-                    <th>
-                        Formato
-                    </th>
-                    <th>
-                        Sugerencias
-                    </th>
-                </tr>
-            </thead>
-            <tr>
-                <td>Lorem ipsun</td>
-                <td>Lorem ipsun</td>
-                <td>Lorem ipsun</td>
-                <td>Lorem ipsun</td>
-                <td>Lorem ipsun</td>
-                <td>Lorem ipsun</td>
-                <td>Lorem ipsun</td>
-                <td>Lorem ipsun</td>
-                <td>Lorem ipsun</td>
-                <td>Lorem ipsun</td>
-            </tr>
-        </table>
-    </div>
 </body>
+
 </html>
